6e603b06300c2a4bfa58c565df45f74efa776c34
JEAN CARLOS CORREA DOS SANTOS <104799740+JeanCarlos-SC@users.noreply.github.com>
2023-04-18 12:40:43 -0300

    fix: fix bugs

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || '3000';
 
 app.listen(PORT, () => {
   console.log(`Runing on ${PORT}`);
